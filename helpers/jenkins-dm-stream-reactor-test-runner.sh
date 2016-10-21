2eeded82a0db1e9a66765961cae9c16606b79ad9
Marios Andreopoulos <opensource@andmarios.com>
2016-10-21 20:34:06 +0300

    Permit helper script to run a bit better locally.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- helpers/jenkins-dm-stream-reactor-test-runner.sh
+++ helpers/jenkins-dm-stream-reactor-test-runner.sh
@@ -80,4 +80,7 @@ mkdir -p mainpage
 wget "https://raw.githubusercontent.com/Landoop/coyote-results-aggregator/master/index.html" -O mainpage/index.html
 sed -e "s/TITLE/$TEST_DIR test/g" -i mainpage/index.html
 
+# Disable error checking in case we run this locally.
+set +e
 cp latest.html "/usr/share/jenkins/public/stream-reactor/$TEST_VERSION/$TEST$(cat "$WORKSPACE"/status.txt).html"
+set -e
